8475b0f3889148a7682440df0c8e31d6cc4f2c50
Laion Corcino <laioncorcino@gmail.com>
2024-04-15 22:08:05 -0300

    [feat] first commit

---
+++ src/main/java/br/uff/uffluir/UffluirApplication.java
@@ -0,0 +1,13 @@
+package br.uff.uffluir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UffluirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UffluirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uff/uffluir/UffluirApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uff.uffluir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UffluirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
